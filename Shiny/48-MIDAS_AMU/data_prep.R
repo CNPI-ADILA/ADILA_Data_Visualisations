28f7cda461ba04e857618fefb0060cd746032a88
Will Cuningham <wcuningh@sgul.ac.uk>
2023-03-15 18:04:15 +0000

    Starts updating data prep due to new data

--- Shiny/48-MIDAS_AMU/data_prep.R
+++ Shiny/48-MIDAS_AMU/data_prep.R
@@ -2,24 +2,23 @@
 data_dir <- "O:/formatted_data/amu_data"  #MIGHT NEED TO USE RELATIVE PATH WHEN DEPLOYING APP ON THE CLOUD (i.e., shinyapps.io) OR AN INTERNAL SERVER???
 
 # load data
-d <- paste0(data_dir,"/all_ddd.csv") #NB: also have data filtered /by_administration and /by_aware categories
+d <- paste0(data_dir,"/row_level_ddd.csv") #NB: also have data filtered /by_administration and /by_aware categories
 data_original <- read.csv(d)
 
 # reformat/clean
   # renaming columns
   data_reformatted <- data_original %>% 
-    select(source_title, country, antimicrobials, aware_category, route_of_administration, effective_from, sum_ddd, sum_did, sum_di) %>%
-    mutate(year = as.numeric(gsub("-01-01", "", effective_from))) %>% select(-effective_from) %>%
-    rename(ddd = sum_ddd, did = sum_did, di = sum_di)
+    select(source_title, who_region, country, antimicrobials, aware_category, route_of_administration, sector, effective_from, standard_units, ddd, did, di) %>%
+    mutate(year = as.numeric(gsub("-01-01", "", effective_from))) %>% select(-effective_from)
   # reshaping wide to long
-  data_reformatted <- gather(data_reformatted, metric, value, ddd, did, di, factor_key = TRUE)
-  # collapsing age_appropriate (true/false) stratification
+  data_reformatted <- gather(data_reformatted, metric, value, standard_units, ddd, did, di, factor_key = TRUE)
+  # collapsing stratification in other categories (e.g., age_appropriate (true/false), manufacturer, etc.)
   data_reformatted <- data_reformatted %>% 
-    group_by(source_title, country, antimicrobials, aware_category, route_of_administration, year, metric) %>%
+    group_by(source_title, who_region, country, antimicrobials, aware_category, route_of_administration, sector, year, metric) %>%
     summarize(value = sum(value, na.rm = TRUE), .groups = "keep") %>%
     ungroup()
 
-# create summed cols for "All" antimicrobials/routes/countries/aware_categories
+# create summed cols for "All" antimicrobials/routes/countries/aware_categories   #ADD WHO_REGION AND SECTOR HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   # "All" in all four groups
   subset_all_4 <- data_reformatted %>% group_by(source_title, year, metric) %>% summarise(value = sum(value, na.rm = TRUE), .groups = "keep") %>% 
     mutate(country = "All", antimicrobials = "All", aware_category = "All", route_of_administration = "All") %>%
@@ -87,7 +86,7 @@ data_for_visualisations <- data_for_visualisations %>%
 
 # make values more manageable 
 data_for_visualisations <- data_for_visualisations %>%
-  mutate(value = case_when(metric == "ddd" ~ value/1000, .default = value))
+  mutate(value = case_when(metric == "ddd" ~ value/1000, metric == "standard_units" ~ value/1000, .default = value))
 
 # aesthetics, functions etc.
   # country polygons 
