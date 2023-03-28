e640eb0b37eb725521fe1530cf8b0545e84a98ac
Will Cuningham <wcuningh@sgul.ac.uk>
2023-03-28 12:28:30 +0100

    Starts work on statistics tab

--- Shiny/48-MIDAS_AMU/statistics.R
+++ Shiny/48-MIDAS_AMU/statistics.R
@@ -1,15 +1,105 @@
-# More tabs under development, for integration into Shiny app:
+# Integrate as new tab ("Statistics") in Shiny app:
 
-# 1. Summary statistics
-  # How many countries/antimicrobials are included in the dataset?
-    # number of antimicrobials per category (e.g., Australia has data for 50 antimicrobials, Access category has 40 antimicrobials,)
-  # For each category and for each tiered combination (except year): 
-    # annual mean/95%CI of standard units/DDD/DID/DI
-    # percentage of total (using the sum of the means)
+########################
+# Data sources summary #
+########################
+  # values to put in a data table
+  num_data_sources <- length(unique(data_for_visualisations$source_title))
+  
+  total_num_countries <- data_for_visualisations
+    
+  avg_num_countries_per_data_source <- data_for_visualisations
+  
+  total_num_antimicrobials <- data_for_visualisations
+    
+  avg_num_antimicrobials_per_data_source <- data_for_visualisations
+    
+  total_year_range <- data_for_visualisations
+    
+  avg_year_range_per_data_source <- data_for_visualisations
+  
+  avg_perc_access <- #these ones will require some collapsing, just use one metric (e.g., DDD), etc.
+  
+  avg_perc_watch <-
+    
+  avg_per_reserve <-
+    
+  avg_perc_oral <-
+  
+  avg_perc_parenteral <-
+    
+    # combine into data table
+    row_names <- c("Number of data sources", etc.)
+    values <- c(num_data_sources, etc.)
+    data_source_summary_table <- data.table("metric" = row_names, "value" = values)
+  
+#------------------------------------------------------------------------------#   
+#------------------------------------------------------------------------------#   
+#------------------------------------------------------------------------------#   
+######################
+# Statistical models #
+######################
+  # DU90 (global)
+    #with colouring by aware category
 
-# 2. Predictive models
+#------------------------------------------------------------------------------#   
+  # Correlations / variation
+    # within...
+      # countries
+        # globally
+          # univariable (i.e., one group at a time)
+            # Is AMC in administration route categories correlated?
+              #imagine line plots of the correlation between oral/parenteral over time for each country, OR,
+              #a scatter plot of oral vs parenteral for all the countries (collapsed by year)
+              
+            # ... same but in aware categories...
+              
+            # ... same but in sectors...
+    
+          # multivariable
+            #e.g., is route correllated to aware correlated to sector?
+    
+        # within WHO regions
+          # ... same as above...
+          
+      # WHO regions
+        # ... same as above...
+    
+      # HIC vs LMIC
+        # ... same as above...
+    
+    # between...
+      # countries
+        # gloabally
+          # univariable (i.e., one group at a time)
+            # Is AMC in administration route categories correlated?
+              
+            # ... same but in aware categories...
+            
+            # ... same but in sectors...
+    
+          # multivariable
+            # ... same as above...
+      
+        # within WHO regions
+          # ... same as above...
+      
+      # WHO regions
+        # ... same as above...
+    
+      # HIC vs LMIC
+        # ... same as above...
+
+#------------------------------------------------------------------------------#   
   # Predict AMC five years into the future/past
     # https://a-little-book-of-r-for-time-series.readthedocs.io/en/latest/src/timeseries.html
     # https://www.pluralsight.com/guides/time-series-forecasting-using-r
     # https://otexts.com/fpp3/
+    
+#------------------------------------------------------------------------------#   
   # Malaria Atlas Project type heat maps?
+    # do they have a package for doing this???
+    
+    
+    
+    
